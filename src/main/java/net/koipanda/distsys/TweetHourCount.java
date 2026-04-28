fa64b70ce9c2b9516778985038218b28ee7dc3fd
Peter Phan <pet3r.ph4n@gmail.com>
2026-04-28 15:26:13 -0500

    remove unused exceptions

--- src/main/java/net/koipanda/distsys/TweetHourCount.java
+++ src/main/java/net/koipanda/distsys/TweetHourCount.java
@@ -47,7 +47,7 @@ public class TweetHourCount {
         private final Map<Integer, Integer> counts = new HashMap<>();
 
         @Override
-        protected void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        protected void reduce(IntWritable key, Iterable<IntWritable> values, Context context) {
             int sum = 0;
             for (IntWritable v : values) {
                 sum += v.get();
